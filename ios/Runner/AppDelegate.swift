a573ab41b65680e312a9ae086246504dfa88a96d
Richard Kweku Aikins <richardkns7@gmail.com>
2023-04-01 23:36:17 +0000

    changes to appdelegate.swift and info.plst

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,6 @@
 import UIKit
 import Flutter
-
+import UserNotifications
 import flutter_local_notifications
 
 @UIApplicationMain
@@ -12,11 +12,23 @@ import flutter_local_notifications
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
-    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
-      GeneratedPluginRegistrant.register(with: registry)
-    }
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        // Pass device token to Flutter to handle
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        let channel = FlutterMethodChannel(name: "com.arksoft.infoctess_koneqt/shedulesChannel", binaryMessenger: window.rootViewController as! FlutterViewController)
+        channel.invokeMethod("onToken", arguments: token)
+    }
+
+    override func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications: \(error.localizedDescription)")
+    }
   }
 }
