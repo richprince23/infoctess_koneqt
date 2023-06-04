498755df209785d1ee91740577851caf4d19cef3
Richard Kweku Aikins <richardkns7@gmail.com>
2023-06-04 18:34:54 +0000

    convo screen media picker dialog done

--- lib/screens/convo_screen.dart
+++ lib/screens/convo_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:infoctess_koneqt/auth.dart';
 import 'package:infoctess_koneqt/components/input_control1.dart';
@@ -22,10 +25,19 @@ class ConvoScreen extends StatefulWidget {
 
 class _ConvoScreenState extends State<ConvoScreen> {
   final msgController = TextEditingController();
+  bool? isTyping;
+  bool isEmpty = true;
+
   @override
   void initState() {
     super.initState();
     markChatAsRead(chatID: widget.chatID);
+    msgController.addListener(() {
+      setState(() {
+        msgController.text.isEmpty ? isTyping = false : isTyping = true;
+        msgController.text.isEmpty ? isEmpty = true : isEmpty = false;
+      });
+    });
   }
 
   @override
@@ -67,13 +79,14 @@ class _ConvoScreenState extends State<ConvoScreen> {
             ),
           ],
         ),
-        actions: [],
+        // actions: [],
       ),
       body: Column(
         children: [
           Expanded(
             flex: 9,
             child: Container(
+              color: cSec.withOpacity(0.05),
               child: StreamBuilder(
                 stream: db
                     .collection("chats")
@@ -121,6 +134,14 @@ class _ConvoScreenState extends State<ConvoScreen> {
             padding: EdgeInsets.all(5.w),
             child: Row(
               children: [
+                IconButton(
+                  onPressed: () {
+                    _showAttachMediaModal(context);
+                  },
+                  icon: const Icon(Icons.add),
+                  iconSize: 24.w,
+                  color: cPri,
+                ),
                 Expanded(
                   child: InputControl(
                     showLabel: false,
@@ -130,11 +151,144 @@ class _ConvoScreenState extends State<ConvoScreen> {
                     isCollapsed: true,
                   ),
                 ),
+                isEmpty == true
+                    ? IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.mic),
+                        iconSize: 24.w,
+                        color: cPri,
+                      )
+                    : IconButton(
+                        style: IconButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: EdgeInsets.all(10.w),
+                          backgroundColor: cPri,
+                          foregroundColor: Colors.white,
+                        ),
+                        onPressed: () async {
+                          if (msgController.text.isNotEmpty) {
+                            // await sendMessage(
+                            //   chatID: widget.chatID,
+                            //   message: msgController.text,
+                            //   senderID: auth.currentUser!.uid,
+                            // );
+                            msgController.clear();
+                          }
+                        },
+                        icon: const Icon(Icons.send),
+                      ),
               ],
             ),
           ),
         ],
       ),
     );
+  }
+
+  // Attach media modal widget
+  Future<void> _showAttachMediaModal(BuildContext context) async {
+    Platform.isIOS
+        ? showCupertinoModalPopup(
+            context: context,
+            builder: (context) => CupertinoActionSheet(
+              title: const Text('Attach Media'),
+              // message: const Text('Select media to attach'),
+              cancelButton: CupertinoActionSheetAction(
+                child: const Text(
+                  'Cancel',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+                  Navigator.pop(context, 'cancel');
+                },
+              ),
+              actions: <Widget>[
+                CupertinoActionSheetAction(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(CupertinoIcons.camera, color: cPri),
+                        SizedBox(
+                          width: 10.w,
+                        ),
+                        const Text('Camera')
+                      ]),
+                  onPressed: () {
+                    Navigator.pop(context, 'Camera');
+                  },
+                ),
+                CupertinoActionSheetAction(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(CupertinoIcons.photo, color: cPri),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      const Text('Gallery'),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, 'Gallery');
+                  },
+                ),
+                CupertinoActionSheetAction(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(CupertinoIcons.doc, color: cPri),
+                      SizedBox(
+                        width: 10.w,
+                      ),
+                      const Text('File'),
+                    ],
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context, 'File');
+                  },
+                ),
+              ],
+            ),
+          )
+        : showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  ListTile(
+                    leading: const Icon(
+                      Icons.camera_alt,
+                    ),
+                    title: const Text('Camera'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.photo),
+                    title: const Text('Gallery'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.video_library),
+                    title: const Text('Video'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.insert_drive_file),
+                    title: const Text('File'),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              );
+            },
+          );
   }
 }
